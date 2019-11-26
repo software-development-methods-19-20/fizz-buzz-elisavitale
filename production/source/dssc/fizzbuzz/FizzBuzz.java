14e138f6a7511bfee793dd5e87f0acc63c2c696c
elisavitale <elisavitale@live.it>
2019-11-26 12:19:29 +0100

    Method fizzBuzz was updated to satisfy tests.

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,13 @@ package dssc.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzz(int number) {
-        return "";
+        if (number % 15 == 0)
+            return "FizzBuzz";
+        else if (number % 3 == 0)
+            return "Fizz";
+        else if (number % 5 == 0)
+            return "Buzz";
+        else
+            return Integer.toString(number);
     }
 }
