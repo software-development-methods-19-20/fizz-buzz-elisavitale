19c8e45fec8c3b51708faf5d9a72ab6f72af5d6b
elisavitale <elisavitale@live.it>
2019-11-26 12:28:56 +0100

    Refactoring: added new method isMultipleOfKey.

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,17 @@ package dssc.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzz(int number) {
-        if (number % 15 == 0)
-            return "FizzBuzz";
-        else if (number % 3 == 0)
+        if (isMultipleOfKey(number, 3) && isMultipleOfKey(number, 5))
+            return "Fizz" + "Buzz";
+        else if (isMultipleOfKey(number, 3))
             return "Fizz";
-        else if (number % 5 == 0)
+        else if (isMultipleOfKey(number, 5))
             return "Buzz";
         else
             return Integer.toString(number);
+    }
+
+    public static boolean isMultipleOfKey(int number, int key) {
+        return number % key == 0;
     }
 }
