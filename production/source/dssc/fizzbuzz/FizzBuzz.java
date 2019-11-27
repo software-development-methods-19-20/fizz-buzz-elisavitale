a2abef706f8a03803929e90a3c1eb851364e7dca
elisavitale <elisavitale@live.it>
2019-11-27 10:00:26 +0100

    Refactoring: the if cycle in method fizzBuzz was condensed in a unique case, by introducing new methods isMultipleOfAnyKey and writeFizzBuzzString.

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -5,18 +5,34 @@ import java.util.HashMap;
 public class FizzBuzz {
 
     public static String fizzBuzz(int number) {
-        if (isMultipleOfKey(number, 3) && isMultipleOfKey(number, 5))
-            return fizzBuzzRules.get(3) + fizzBuzzRules.get(5);
-        else if (isMultipleOfKey(number, 3))
-            return fizzBuzzRules.get(3);
-        else if (isMultipleOfKey(number, 5))
-            return fizzBuzzRules.get(5);
+        if (isMultipleOfAnyKey(number))
+            return writeFizzBuzzString(number);
         else
             return Integer.toString(number);
     }
 
+    private static boolean isMultipleOfAnyKey(int number) {
+        boolean isMultiple = false;
+        for (int key : fizzBuzzRules.keySet()) {
+            isMultiple = isMultiple || isMultipleOfKey(number, key);
+        }
+        return isMultiple;
+    }
+
     private static boolean isMultipleOfKey(int number, int key) {
         return number % key == 0;
+    }
+
+    private static String writeFizzBuzzString(int number) {
+        StringBuilder fbstring = new StringBuilder();
+        for (int key : fizzBuzzRules.keySet())
+            if (isMultipleOfKey(number, key))
+                updateStringValue(fbstring, key);
+        return fbstring.toString();
+    }
+
+    private static void updateStringValue(StringBuilder fbstring, int key) {
+        fbstring.append(fizzBuzzRules.get(key));
     }
 
     private static HashMap<Integer, String> fizzBuzzRules = putRules();
