b49e981ee86284fb579125beef0414abd74af7d9
elisavitale <elisavitale@live.it>
2019-11-27 09:31:55 +0100

    Refactoring: added new HashMap with conversion rules.

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,30 @@
 package dssc.fizzbuzz;
 
+import java.util.HashMap;
+
 public class FizzBuzz {
 
     public static String fizzBuzz(int number) {
         if (isMultipleOfKey(number, 3) && isMultipleOfKey(number, 5))
-            return "Fizz" + "Buzz";
+            return fizzBuzzRules.get(3) + fizzBuzzRules.get(5);
         else if (isMultipleOfKey(number, 3))
-            return "Fizz";
+            return fizzBuzzRules.get(3);
         else if (isMultipleOfKey(number, 5))
-            return "Buzz";
+            return fizzBuzzRules.get(5);
         else
             return Integer.toString(number);
     }
 
-    public static boolean isMultipleOfKey(int number, int key) {
+    private static boolean isMultipleOfKey(int number, int key) {
         return number % key == 0;
+    }
+
+    private static HashMap<Integer, String> fizzBuzzRules = putRules();
+
+    private static HashMap<Integer, String> putRules() {
+        HashMap<Integer, String> rules = new HashMap<>();
+        rules.put(3, "Fizz");
+        rules.put(5, "Buzz");
+        return rules;
     }
 }
